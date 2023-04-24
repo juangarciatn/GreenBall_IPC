b977679338ba462c36ca7ce5ce7a4b40b3d6c08a
Juan García <91282527+juangarciatn@users.noreply.github.com>
2023-04-24 20:52:42 +0200

    Base proyecto GreenBall
    He cambiado todo, estructurando la aplicación y los controladores de las ventanas emergentes, login y signup.

---
+++ IPC_GreenBall/src/application/GreenBallApplication.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package application;
+
+/**
+ *
+ * @author Juan
+ */
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author jsanchez
+ * Modificado jsoler
+ */
+public class GreenBallApplication extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader= new FXMLLoader(getClass().getResource("/startPage/FXMLstartPage.fxml"));
+        Parent root = loader.load();
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.setTitle("Demo vista de lista de personas");
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
+
